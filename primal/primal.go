3d2e9b1fb510406d1110ff8fe13f5cc846dd1344
George Spiceland <georgespiceland@gmail.com>
2016-08-14 00:56:28 -0400

    Miller-Rabin deterministic test completely hosed. Implementation fails entirely. Needs much more work and possibly a total rethink.

--- primal/primal.go
+++ primal/primal.go
@@ -8,15 +8,16 @@ import (
 func IsPrime(primed int) (isPrime bool) {
 	isComp := fermat(primed) //first use fermat to check for compositeness
 	if isComp == true {
-		isPrime == false
+		isPrime = false
 		return
 	} //if isComp == true
 	isPrime = determine(primed)
+	return
 } //func IsPrime
 
 func fermat(fermatted int) (isComp bool) {
 	if fermatted > 1 {
-		little := (int(math.Pow(2, (fermatted - 1)))) % fermatted
+		little := (int(math.Pow(float64(2), (float64(fermatted-1)))) % fermatted)
 		if little != 1 {
 			isComp = true
 			return
@@ -24,27 +25,27 @@ func fermat(fermatted int) (isComp bool) {
 		isComp = false
 		return
 	} //if fermatted > 1
+	return
 } //func fermat
 
 func determine(determined int) (isPrime bool) {
 	//TODO: This area is a construction site ###
 	d := ((determined - 1) / 4) //This matches to an s of 2
-	if (int(math.Pow(2, d))%determined != (1 || determined-1)) &&
-		(int(math.Pow(2, d*2))%determined == (determined - 1)) &&
-		(int(math.Pow(3, d))%determined != (1 || determined-1)) &&
-		(int(math.Pow(3, d*2))%determined == (determined - 1)) &&
-		(int(math.Pow(5, d))%determined != (1 || determined-1)) &&
-		(int(math.Pow(5, d*2))%determined == (determined - 1)) &&
-		(int(math.Pow(7, d))%determined != (1 || determined-1)) &&
-		(int(math.Pow(7, d*2))%determined == (determined - 1)) &&
-		(int(math.Pow(11, d))%determined != (1 || determined-1)) &&
-		(int(math.Pow(11, d*2))%determined == (determined - 1)) {
+	if int(math.Pow(float64(2, d))%determined != (1 || determined-1)) &&
+		(int(math.Pow(float64(2), float64(d*2)))%determined == (determined - 1)) &&
+		(int(math.Pow(float64(3), float64(d)))%determined != (1 || determined-1)) &&
+		(int(math.Pow(float64(3), float64(d*2)))%determined == (determined - 1)) &&
+		(int(math.Pow(float64(5), float64(d)))%determined != (1 || determined-1)) &&
+		(int(math.Pow(float64(5), float64(d*2)))%determined == (determined - 1)) &&
+		(int(math.Pow(float64(7), float64(d)))%determined != (1 || determined-1)) &&
+		(int(math.Pow(float64(7), float64(d*2)))%determined == (determined - 1)) &&
+		(int(math.Pow(float64(11), float64(d)))%determined != (1 || determined-1)) &&
+		(int(math.Pow(float64(11), float64(d*2)))%determined == (determined - 1)) {
 
 		isPrime = true
 		return
-	} else {
-		isPrime = false
-		return
 	}
+	isPrime = false
+	return
 	//	This area is a construction site ###
 } //func determine
